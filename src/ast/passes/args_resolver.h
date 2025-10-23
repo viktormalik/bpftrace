86243f72d5de9f5b45f98643184356b932aeceda
Viktor Malik <viktor.malik@gmail.com>
2025-10-23 11:24:24 +0200

    Use Result in argument resolution
    Most functions for resolving arguments from BTF return
    std::shared_ptr<Struct>. In case the resolution fails, they return
    nullptr and pass the error message via an argument.
    
    Use the Result class instead since this is its exact purpose. Refactor
    ArgsResolverPass to consume the Result.
    
    No functional change.
    
    Signed-off-by: Viktor Malik <viktor.malik@gmail.com>

--- src/ast/passes/args_resolver.h
+++ src/ast/passes/args_resolver.h
@@ -5,6 +5,20 @@
 
 namespace bpftrace::ast {
 
+class ProbeArgParseError : public ErrorInfo<ProbeArgParseError> {
+public:
+  static char ID;
+  ProbeArgParseError(std::string probe_name, std::string &&detail)
+      : probe_name_(std::move(probe_name)), detail_(std::move(detail)) {};
+  ProbeArgParseError(std::string_view probe_name, std::string &&detail)
+      : ProbeArgParseError(std::string(probe_name), std::move(detail)) {};
+  void log(llvm::raw_ostream &OS) const override;
+
+private:
+  std::string probe_name_;
+  std::string detail_;
+};
+
 Pass CreateArgsResolverPass(std::vector<ProbeType>&& probe_types = {
     ProbeType::fentry,
     ProbeType::fexit,
